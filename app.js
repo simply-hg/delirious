3a3b3be645542f748b88b141b2a9c4ee60745227
simply-hg <hansgeorg.kluge@gmail.com>
2013-03-24 22:40:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 var fm_key, fm_url, fm_user;
-var groups = {};
-var feeds = {};
+
+var groups       = {};
+var feeds        = {};
 var feeds_groups = {};
-var favicons = {};
-var items = [];
-var saved_items = [];
-var session_unread_items = [];
-var called_group  = false;
-var called_saved  = false;
-var called_feed   = false;
-var called_sparks = false;
-var called_hot    = false;
+var favicons     = {};
+
+var items              = [];
+var saved_items        = [];
+var session_read_items = [];
+
+// For navigation purposes to refresh listviews
+var called_group     = false;
+var called_saved     = false;
+var called_feed      = false;
+var called_sparks    = false;
+var called_hot       = false;
 var called_all_feeds = false;
 var called_kindling  = false;
-var loading = 0;
-var auth_success = false;
-var last_fmjs_refresh = 0;
+
+var loading           = 0; // counts current loading processes
+var auth_success      = false; // if a successful auth has been registered this is true. helps on stopped or failed connections. Not (!) used for authentification
+var last_fmjs_refresh = 0; // unix timestamps in seconds when were items refreshed last time
 
 function start() {
 	// Load Config, if any
@@ -33,6 +38,7 @@ function start() {
 		if ( checkAuth(data.auth) ) {
 			auth_success = true;
 			// Get groups and build them
+			compareSavedItems("start");
 			showHideLoader("start");
 			$.post(fm_url + "?api&groups", { api_key: fm_key }).done(function(data) {
 				showHideLoader("stop");
@@ -110,6 +116,7 @@ function createGroups(refresh) {
 			showHideLoader("stop");
 
 			if ( checkAuth(data.auth) ) {
+				feeds_groups = data.feeds_groups;
 				groups       = _.sortBy(data.groups, "title");
 				createGroups(false);
 			}
@@ -129,17 +136,19 @@ function createGroups(refresh) {
 function getSettings() {
 	if ( $.jStorage.storageAvailable() ) {
 		fm_key   = $.jStorage.get("fmjs-key", "");
-		fm_url   = $.jStorage.get("fmjs-url", "none");
-		fm_user  = $.jStorage.get("fmjs-user", "none");
+		fm_url   = $.jStorage.get("fmjs-url", "");
+		fm_user  = $.jStorage.get("fmjs-user", "");
 		favicons = $.jStorage.get("fmjs-favicons");
 		
-		if ( fm_url != "none" ) {
+		saved_items = $.jStorage.get("fmjs-local-items", []);
+		
+		if ( fm_url != "" ) {
 			$("#fmjs-fever-url").val(fm_url);
 		}
-		if ( fm_user != "none" ) {
+		if ( fm_user != "" ) {
 			$("#fmjs-e-mail").val(fm_user);
 		}
-		
+
 	} else {
 		return false;
 	}
@@ -163,9 +172,9 @@ function saveSettings() {
 	} else {
 		return false;
 	}
-
-	$.mobile.changePage("#page-home", {transition: "slide"});
 	start();
+	$.mobile.changePage("#page-home", {transition: "slide"});
+	
 }
 
 function refreshSavedItems() {
@@ -174,12 +183,22 @@ function refreshSavedItems() {
 		showHideLoader("stop");
 		if ( checkAuth(data.auth) ) {
 			$.jStorage.set("fmjs-local-items", []);
+			saved_items = [];
 			if ( data.saved_item_ids != "") {
 				var ids = data.saved_item_ids.split(',');
+				console.log("Anzahl saved: " + ids.length);
+				processLoadedSaveItems = _.after(ids.length, storeLoadedSavedItems);
 				loadSavedItems(ids);
 			}
 		}
 	}).fail(function(){ showHideLoader("stop"); checkAuth(0); });
+}
+
+function storeLoadedSavedItems() {
+	saved_items = _.sortBy(saved_items, "created_on_time");
+	$.jStorage.set("fmjs-local-items", saved_items);
+	//console.log(saved_items);
+	console.log("Locally saved " + saved_items.length + " items");
 }
 
 function loadSavedItems(ids) {
@@ -198,9 +217,10 @@ function loadSavedItems(ids) {
 		showHideLoader("stop");
 		if ( checkAuth(data.auth) ) {
 			$.each(data.items, function(index, value) {
-				var local_items = $.jStorage.get("fmjs-local-items", []);
-				local_items.push(value);
-				$.jStorage.set("fmjs-local-items", local_items);
+				//var local_items = $.jStorage.get("fmjs-local-items", []);
+				saved_items.push(value);
+				processLoadedSaveItems();
+				//$.jStorage.set("fmjs-local-items", local_items);
 			});
 		}
 	}).fail(function(){ showHideLoader("stop"); checkAuth(0); });
@@ -216,24 +236,22 @@ function loadSavedItems(ids) {
 function showSaved() {
 	$("#fmjs-saved-content").empty();
 	$("#fmjs-saved-content").append('<ul id="fmjs-saved-view" data-role="listview" data-divider-theme="d" data-inset="true" data-filter="true"></ul>');
-	var local_items = $.jStorage.get("fmjs-local-items", []);
-	console.log(local_items);
-	if ( local_items.length > 0 ) {
-		local_items = _.sortBy(local_items, "created_on_time");
-		$.each(local_items, function(index, value) {
-			//if ( value.is_saved == "1" ) {
-
-				var item = "";
-				item += '<li data-theme="c"><p>';
+	//var local_items = $.jStorage.get("fmjs-local-items", []);
+	//console.log(local_items);
+	if ( saved_items.length > 0 ) {
+		//local_items = _.sortBy(local_items, "created_on_time");
+		$.each(saved_items, function(index, value) {
+			var item = "";
+			item += '<li data-theme="c"><p>';
 							
-				var feedname = _.findWhere(feeds, {id: value.feed_id});
-				item += getFavicon(feedname);
-						
-				item += '<a href="" class="fmjs-hot-links fmjs-item-is-read" onclick="showSingleItem('+value.id+');">' + value.title + '</a>';
-				item += ' by <a href="" class="fmjs-hot-links" onclick="showFeed('+value.feed_id+');">'+feedname.title+'</a></p>';
-				item += '</li>';
-				$("#fmjs-saved-view").append(item);
-			//}
+			var feedname = _.findWhere(feeds, {id: value.feed_id});
+			item += getFavicon(feedname);
+					
+			item += '<a href="" class="fmjs-hot-links fmjs-item-is-read" onclick="showSingleItem('+_.escape(value.id)+');">' + _.escape(value.title) + '</a>';
+			item += ' by <a href="" class="fmjs-hot-links" onclick="showFeed('+_.escape(value.feed_id)+');">'+_.escape(feedname.title)+'</a></p>';
+			item += '</li>';
+			$("#fmjs-saved-view").append(item);
+
 		});
 	}
 	if (called_saved == false ) {
@@ -250,12 +268,12 @@ function showHot(page) {
 	if ( page == 1 ) {
 		// First page has been called, so do a complete refresh
 		$("#fmjs-hot-content").empty();
-		$("#fmjs-hot-more").attr("onclick", "showHot(2)");
+		$("#fmjs-hot-more").attr("onclick", "showHot(2);");
 	} else {
 		// another page has been called, so let's append a new one
 		var next_page = page;
 		next_page++;
-		$("#fmjs-hot-more").attr("onclick", "showHot("+next_page+")");
+		$("#fmjs-hot-more").attr("onclick", "showHot("+next_page+");");
 	}
 	
 	// Get range and offset
@@ -287,23 +305,24 @@ function showHot(page) {
 				if (value.is_local == 1 && value.is_item == 1) {
 					// Add local stuff here, like excerpt an feed name.
 					
-					item +='<p style="max-height:2.5em;overflow:hidden;" class="fmjs-link-'+value.item_id+'-content"></p>';
-					item += '<p style="text-align:right;">posted by <span class="fmjs-link-'+value.item_id+'-favicon"></span> <span class="fmjs-link-'+value.item_id+'-feedname fmjs-hot-links">Feed</span></p><p style="text-align:right;"><a href="'+value.url+'" target="_blank" data-role="button" data-theme="b" data-inline="true" data-mini="true" class="fmjs-hot-to-button" data-icon="grid">Open URL</a> <a href="#" onclick="saveItem('+value.item_id+');" target="_blank" data-role="button" data-icon="star" data-theme="b" data-inline="true" data-mini="true" class="fmjs-hot-to-button">Save</a></p>';
+					item +='<p style="max-height:2.5em;overflow:hidden;" class="fmjs-link-'+_.escape(value.item_id)+'-content"></p>';
+					item += '<p style="text-align:right;">posted by <span class="fmjs-link-'+_.escape(value.item_id)+'-favicon"></span> <span class="fmjs-link-'+_.escape(value.item_id)+'-feedname fmjs-hot-links">Feed</span></p><p style="text-align:right;"><a href="'+_.escape(value.url)+'" target="_blank" data-role="button" data-theme="b" data-inline="true" data-mini="true" class="fmjs-hot-to-button" data-icon="grid">Open URL</a> <a href="#" onclick="saveItem('+_.escape(value.item_id)+');" target="_blank" data-role="button" data-icon="star" data-theme="b" data-inline="true" data-mini="true" class="fmjs-hot-to-button">Save</a></p>';
 				}
 			
 				// Now we show a list of all those items, linking to this hot item...
-				item += '<ul data-role="listview" data-divider-theme="d" data-inset="true" id="fmjs-hot-content-link-'+value.id+'" class="fmjs-hot-linkbox fmjs-to-listview">';
+				item += '<ul data-role="listview" data-divider-theme="d" data-inset="true" id="fmjs-hot-content-link-'+_.escape(value.id)+'" class="fmjs-hot-linkbox fmjs-to-listview">';
 
 				var links = value.item_ids.split(',');
 				for (var i=0, link_id; link_id=links[i]; i++) {
 					// item is "some", then "example", then "array"
 					// i is the index of item in the array
+					link_id = _.escape(link_id);
 					item += '<li><p><span class="fmjs-link-'+link_id+'-favicon"></span><a href="#" class="fmjs-link-'+link_id+' fmjs-hot-links fmjs-single-item-link-'+link_id+'" onclick="showSingleItem('+link_id+');"><span class="fmjs-link-'+link_id+'-title fmjs-hot-links fmjs-single-item-link-'+link_id+'">Link: '+link_id+'</span></a> by <span class="fmjs-link-'+link_id+'-feedname fmjs-hot-links">Feed</span></p></li>';
 					id_list += link_id + ",";
 				}	
 				item += '</ul>';
 				//
-				item += '<div style="text-align:right"><a href="#" data-role="button" onclick="markItemsRead(\''+id_list+'\');" data-theme="b" data-inline="true" data-mini="true" class="fmjs-hot-to-button" data-icon="check">Mark Links as read</a></div>';
+				item += '<div style="text-align:right"><a href="#" data-role="button" onclick="markItemsRead(\''+_.escape(id_list)+'\');" data-theme="b" data-inline="true" data-mini="true" class="fmjs-hot-to-button" data-icon="check">Mark Links as read</a></div>';
 				//
 				item += '</div>';
 				$("#fmjs-hot-content").append(item);
@@ -313,7 +332,7 @@ function showHot(page) {
 			});	
 
 			var ids_to_get = load_ids.split(',');
-			ids_to_get = _.uniq(ids_to_get);
+			ids_to_get     = _.uniq(ids_to_get);
 			
 			fillLinkPlaceholder(ids_to_get, 'link' );
 			if ( page == 1) {
@@ -357,7 +376,7 @@ function fillLinkPlaceholder(placeholder_ids, class_prefix) {
 
 				var feedname = _.findWhere(feeds, {id: value.feed_id});
 
-				$(".fmjs-"+class_prefix+"-"+value.id+"-feedname").html('<a href="#" onclick="showFeed('+$.trim(feedname.id)+');">'+_.escape(feedname.title)+'</a>');
+				$(".fmjs-"+class_prefix+"-"+value.id+"-feedname").html('<a href="#" onclick="showFeed('+_.escape($.trim(feedname.id))+');">'+_.escape(feedname.title)+'</a>');
 
 				var favicon = getFavicon(feedname);
 				$(".fmjs-"+class_prefix+"-"+value.id+"-favicon").append(favicon).removeClass("fmjs-"+class_prefix+"-"+value.id+"-favicon");
@@ -400,8 +419,8 @@ function showGroup(id) {
 			var feed = _.findWhere(feeds, {id: value.feed_id});
 			item += getFavicon(feed);
 
-			item += '<a href="#" onclick="showSingleItem('+value.id+')" class="fmjs-hot-links fmjs-item-is-unread fmjs-single-item-link-'+value.id+'">' + value.title + '</a>';
-			item += ' by <a href="#" onclick="showFeed('+feed.id+');" class="fmjs-hot-links">'+feed.title+'</a>';
+			item += '<a href="#" onclick="showSingleItem('+_.escape(value.id)+')" class="fmjs-hot-links fmjs-item-is-unread fmjs-single-item-link-'+_.escape(value.id)+'">' + _.escape(value.title) + '</a>';
+			item += ' by <a href="#" onclick="showFeed('+_.escape(feed.id)+');" class="fmjs-hot-links">'+_.escape(feed.title)+'</a>';
 			item += '</p></li>';
 			item_ids_in_group += value.id +",";
 			$("#fmjs-group-view").append(item);
@@ -459,7 +478,7 @@ function showFeed(id) {
 			
 			item += getFavicon(called_feed_info);
 
-			item += '<a href="#" class="fmjs-hot-links fmjs-item-is-unread fmjs-single-item-link-'+value.id+'" onclick="showSingleItem('+value.id+');">' + value.title + '</a>';
+			item += '<a href="#" class="fmjs-hot-links fmjs-item-is-unread fmjs-single-item-link-'+_.escape(value.id)+'" onclick="showSingleItem('+_.escape(value.id)+');">' + _.escape(value.title) + '</a>';
 			item += '</p></li>';
 			$("#fmjs-feed-view").append(item);
 		}
@@ -492,19 +511,38 @@ function refreshFavicons() {
 function showSingleItem(id) {
 	var item = _.findWhere(items, {id: id});
 	//conso-le.log(item);
-	session_unread_items.push(item);
-	console.log(session_unread_items);
+	
+	//console.log(session_read_items);
 	if ( item ) {
+		session_read_items.push(item);
 		console.log("cache hit");
 		renderSingleItem(item);
 	} else {
-		showHideLoader("start");
-		$.post(fm_url + "?api&items&with_ids="+ _.escape(id), { api_key: fm_key }).done(function(data) {
-			showHideLoader("stop");
-			if ( checkAuth(data.auth) ) {
-				renderSingleItem(data.items[0]);
+		// Could be a saved item...
+		item = _.findWhere(saved_items, {id: id});
+		if ( item ) {
+			console.log("saved item hit");
+			renderSingleItem(item);
+		} else {
+			// Let's check for unread_cache
+			item = _.findWhere(session_read_items, {id: id});
+			if ( item ) {
+				console.log("unread_cache hit");
+				renderSingleItem(item);
+			} else {
+				// Nothing, so load item
+				console.log("Loading item");
+				showHideLoader("start");
+				$.post(fm_url + "?api&items&with_ids="+ _.escape(id), { api_key: fm_key }).done(function(data) {
+					showHideLoader("stop");
+					if ( checkAuth(data.auth) ) {
+						session_read_items.push(data.items[0]);
+						renderSingleItem(data.items[0]);
+					}
+				}).fail(function(){ showHideLoader("stop"); checkAuth(0); });			
 			}
-		}).fail(function(){ showHideLoader("stop"); checkAuth(0); });
+		}
+
 	}
 }
 
@@ -546,8 +584,8 @@ function renderSingleItem(data) {
 	var favicon_img = getFavicon(feedname);
 
 	$("#fmjs-single-feedname").html(favicon_img + _.escape(feedname.title));
-	$("#fmjs-single-feedname").attr("onclick", "showFeed("+data.feed_id+");");
-	console.log(data.id);
+	$("#fmjs-single-feedname").attr("onclick", "showFeed("+_.escape(data.feed_id)+");");
+
 	markItemsRead(data.id.toString());
 	if ( data.is_saved == 1 ) {
 		$("#fmjs-single-btn-save .ui-btn-text").html("Unsave");
@@ -589,7 +627,6 @@ function markItemsRead(ids) {
 			return true;
 		}
 	});
-	
 
 	$.each(ids_to_mark_read, function(index, value) {
 		markItemRead(value);
@@ -655,30 +692,22 @@ function saveItem(id) {
 	id = $.trim(id);
 	if ( id != "") {
 
-		console.log("checkpoint 1");
-		var local_items = $.jStorage.get("fmjs-local-items", []);
+		//var local_items = $.jStorage.get("fmjs-local-items", []);
 		var item = _.findWhere(items, {id: id});
-		console.log("checkpoint 2");
-		console.log(item);
+
 		if ( item ) {
 			//
-			console.log("checkpoint fail in this case");
-			local_items.push(item);
-			$.jStorage.set("fmjs-local-items", local_items);
+			item.is_saved = 1;
+			saved_items.push(item);
+			$.jStorage.set("fmjs-local-items", saved_items);
 		} else {
-			console.log("checkpoint 3");
-			console.log(session_unread_items);
 
-			item = _.findWhere(session_unread_items, {id: id});
-			console.log("checkpoint 4");
-			//console.log(item);					
+			item = _.findWhere(session_read_items, {id: id});
+			
 			if ( item ) {
-				console.log("checkpoint 5");
-				local_items.push(item);
-				
-				$.jStorage.set("fmjs-local-items", local_items);
-				var kjsdgfbjkdf = $.jStorage.get("fmjs-local-items", []);
-				console.log(kjsdgfbjkdf);
+				item.is_saved = 1;
+				saved_items.push(item);
+				$.jStorage.set("fmjs-local-items", saved_items);
 			}
 			
 		}
@@ -694,7 +723,7 @@ function saveItem(id) {
 
 function unsaveItem(id) {
 	if ( $.trim(id) != "") {
-		var saved_items = $.jStorage.get("fmjs-local-items", []);
+		//var saved_items = $.jStorage.get("fmjs-local-items", []);
 		current_unsave_id = id;
 		saved_items = _.reject(saved_items, function(item) {
 			if ( item.id != current_unsave_id )  {
@@ -759,7 +788,7 @@ function showSparks() {
 			var feed = _.findWhere(feeds, {id: value.feed_id});
 			item += getFavicon(feed);
 			
-			item += '<a href="#" onclick="showSingleItem('+value.id+')" class="fmjs-hot-links fmjs-item-is-unread fmjs-single-item-link-'+value.id+'">' + value.title + '</a>';
+			item += '<a href="#" onclick="showSingleItem('+_.escape(value.id)+')" class="fmjs-hot-links fmjs-item-is-unread fmjs-single-item-link-'+_.escape(value.id)+'">' + _.escape(value.title) + '</a>';
 			item += '</p></li>';
 			item_ids_in_sparks += value.id +",";
 			$("#fmjs-sparks-view").append(item);
@@ -796,11 +825,11 @@ function showAllFeeds() {
 
 		var item = '';
 		item += '<li>';
-		item += '<a onclick="showFeed('+value.id+')">';
+		item += '<a onclick="showFeed('+_.escape(value.id)+')">';
 		
 		item += getFavicon(value, "ui-li-icon ui-corner-none");
 		
-		item += value.title+'</a>';
+		item += _.escape(value.title)+'</a>';
 		item += '</li>';
 
 		$("#fmjs-all-feeds-view").append(item);
@@ -849,8 +878,8 @@ function showKindling() {
 			var feed = _.findWhere(feeds, {id: value.feed_id});
 			item += getFavicon(feed);
 
-			item += '<strong><a href="#" onclick="showSingleItem('+value.id+')" class="fmjs-hot-links fmjs-item-is-unread fmjs-single-item-link-'+value.id+'">' + value.title + '</a></strong>';
-			item += ' by <a href="#" onclick="showFeed('+feed.id+');" class="fmjs-hot-links">'+feed.title+'</a>';
+			item += '<strong><a href="#" onclick="showSingleItem('+_.escape(value.id)+')" class="fmjs-hot-links fmjs-item-is-unread fmjs-single-item-link-'+_.escape(value.id)+'">' + _.escape(value.title) + '</a></strong>';
+			item += ' by <a href="#" onclick="showFeed('+_.escape(feed.id)+');" class="fmjs-hot-links">'+_.escape(feed.title)+'</a>';
 			item += '</p></li>';
 
 			$("#fmjs-kindling-view").append(item);
@@ -886,4 +915,68 @@ function getFavicon(feed, css_classes) {
 	
 	// return generic code
 	return '<img src="feed-icon-generic.png" height="16" width="16" class="'+css_classes+'"/>';
+}
+
+function showHome() {
+
+}
+
+function countUnread() {
+
+}
+
+function compareSavedItems(what) {
+	// This function compares your local stored items with all saved items online.
+	// if some are missing here, we load them.
+	// if some are missing there, we remove our copy (beacuse it was probably unsaved somewhere else...).
+	// It is done by comparing ids
+	
+	if ( what == "full" ) {
+		refreshSavedItems();
+		return;
+	}
+	showHideLoader("start");
+	$.post(fm_url + "?api&saved_item_ids", { api_key: fm_key }).done(function(data) {
+		showHideLoader("stop");
+		if ( checkAuth(data.auth) ) {
+			var online_ids = data.saved_item_ids.split(',');
+			console.log("Anzahl saved online: " + online_ids.length);
+			local_ids = [];
+			$.each(saved_items, function(index, value) {
+				local_ids.push(value.id.toString());
+			});
+			console.log("Anzahl local: " + local_ids.length);
+			var load_em = _.difference(online_ids, local_ids);
+			console.log(load_em);
+			delete_em =  _.difference(local_ids, online_ids);
+			console.log(delete_em);
+			
+			if ( load_em.length > 0 ) {
+				if ( what == "start" && load_em.length > 50 ) {
+					// too much to load on start, this should be done with a full refresh...
+					// adding a message to top
+					$("#fmjs-homescreen-message").append('<div class="warning">Please reload your saved items in the settings screen (click to hide message).</div>');
+					return;
+					
+				} else {
+					processLoadedSaveItems = _.after(load_em.length, storeLoadedSavedItems);
+					loadSavedItems(load_em);
+				}
+			}
+			
+			if ( delete_em.length > 0 ) {
+				saved_items = _.reject(items, function(item) {
+	
+					if ( $.inArray(item.id.toString(), delete_em ) == -1 )  {
+						return false;
+					} else {
+						console.log("reject");
+						return true;
+					}
+				});
+			}
+			$.jStorage.set("fmjs-local-items", saved_items);
+
+		}
+	}).fail(function(){ showHideLoader("stop"); checkAuth(0); });
 }
